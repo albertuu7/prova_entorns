f29050b2a640da3763b46de10d82fd30012c9028
albertogonzalez <albertogonzalez@192.168.0.163>
2017-05-16 17:50:59 +0200

    Verió en Valencià

--- prova_alberto/src/prova_alberto/Prova_alberto.java
+++ prova_alberto/src/prova_alberto/Prova_alberto.java
@@ -15,7 +15,8 @@ public class Prova_alberto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //VALENCIÀ
+        System.out.println("Hola Mon");
     }
     
 }
